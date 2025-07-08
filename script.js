bc0d4c243ab410c42b31500cd23fd0cd50b319b9
Avaz Hojakeldiyev <azho.dev@gmail.com>
2025-07-08 21:25:47 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,104 @@
+// const API_KEY = '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';
+// variant 2
+const API_KEY = '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';
+
+
+// const API_KEY = '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';
+
+
+const content = document.getElementById('content');
+const chatInput = document.getElementById('chatInput');
+const sendButton = document.getElementById('sendButton');
+
+let isAnswerLoading = false;
+let answerSectionId = 0;
+
+sendButton.addEventListener('click', () => handleSendMessage());
+chatInput.addEventListener('keypress', (event) => {
+    if (event.key === 'Enter') {
+        handleSendMessage()
+    }
+})
+
+function handleSendMessage() {
+    const question = chatInput.value.trim();
+
+    if (question === '' || isAnswerLoading) return
+
+    sendButton.classList.add('send-button-nonactive');
+
+    addQuestionSection(question);
+    chatInput.value = ''
+}
+
+function getAnswer(question) {
+    const fetchData =
+        fetch("https://openrouter.ai/api/v1/chat/completions", {
+            method: "POST",
+            headers: {
+                "Authorization": `Bearer ${API_KEY}`,
+                "Content-Type": "application/json",
+                "HTTP-Referer": "https://example.com", // ← добавь свой домен или localhost
+                "X-Title": "MyStudyProject"
+            },
+            body: JSON.stringify({
+                "model": "deepseek/deepseek-r1-0528:free",
+                "messages": [
+                    {
+                        "role": "user",
+                        "content": question
+                    }
+                ]
+            })
+        });
+
+    fetchData.then(response => response.json())
+        .then(data => {
+            const resultData = data.choices[0].message.content;
+            isAnswerLoading = false;
+            addAnswerSection(resultData)
+        }).finally(() => {
+            sendButton.classList.remove('send-button-nonactive');
+            scrollToBottom();
+        })
+}
+
+
+function addQuestionSection(message) {
+    isAnswerLoading = true;
+    const sectionElement = document.createElement('section');
+    sectionElement.className = 'question-section';
+    sectionElement.textContent = message;
+
+    content.appendChild(sectionElement);
+
+    addAnswerSection(message);
+    scrollToBottom();
+}
+
+function addAnswerSection(message) {
+    if (isAnswerLoading) {
+        answerSectionId++;
+        const sectionElement = document.createElement('section');
+        sectionElement.className = 'answer-section';
+        sectionElement.innerHTML = getLoadingSvg();
+        sectionElement.id = answerSectionId;
+
+        content.appendChild(sectionElement);
+        getAnswer(message)
+    } else {
+        const answerSectionElement = document.getElementById(answerSectionId);
+        answerSectionElement.textContent = message;
+    }
+}
+
+function getLoadingSvg() {
+    return `<svg style="height: 25px" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 200 200"><circle fill="#4F6BFE" stroke="#4F6BFE" stroke-width="15" r="15" cx="40" cy="100"><animate attributeName="opacity" calcMode="spline" dur="2" values="1;0;1;" keySplines=".5 0 .5 1;.5 0 .5 1" repeatCount="indefinite" begin="-.4"></animate></circle><circle fill="#4F6BFE" stroke="#4F6BFE" stroke-width="15" r="15" cx="100" cy="100"><animate attributeName="opacity" calcMode="spline" dur="2" values="1;0;1;" keySplines=".5 0 .5 1;.5 0 .5 1" repeatCount="indefinite" begin="-.2"></animate></circle><circle fill="#4F6BFE" stroke="#4F6BFE" stroke-width="15" r="15" cx="160" cy="100"><animate attributeName="opacity" calcMode="spline" dur="2" values="1;0;1;" keySplines=".5 0 .5 1;.5 0 .5 1" repeatCount="indefinite" begin="0"></animate></circle></svg>`
+}
+
+function scrollToBottom() {
+    content.scrollTo({
+        top: content.scrollHeight,
+        behavior: "smooth",
+    })
+}
